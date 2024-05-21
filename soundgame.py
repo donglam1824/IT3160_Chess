8a03d1553ad08eb6f9744c3f358133eb315b2025
Bphuc2105 <163286618+Bphuc2105@users.noreply.github.com>
2024-05-21 19:13:16 +0700

    Update soundgame.py

--- soundgame.py
+++ soundgame.py
@@ -4,7 +4,7 @@ import pygame
 class SoundManager:
     def __init__(self):
         pygame.mixer.init()
-        self.background_music = pygame.mixer.Sound(r"C:\Users\BaoPhuc\Downloads\sound.mp3")
+        self.background_music = pygame.mixer.Sound(r"Chess_Image/sound.mp3")
 
     def play_background_music(self, loops=-1):
         self.background_music.play(loops=loops)
