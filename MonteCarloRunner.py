6d9886183ed4908ddead0c193b16f61d06c80306
isjustaname <50333654+isjustaname@users.noreply.github.com>
2024-06-07 11:01:29 +0700

    database update

--- MonteCarloRunner.py
+++ MonteCarloRunner.py
@@ -12,7 +12,7 @@ my_repo = git.Repo(os.getcwd())
 if my_repo.is_dirty(untracked_files=True):
     print('Changes detected.')
     # print(my_repo.git.diff(my_repo.head.commit.tree))
-    my_repo.index.add('MonteCarloRunner.py', "\MonteCarlo\database.db")
+    my_repo.index.add('MonteCarloRunner.py',open("MonteCarlo\database.db", "r"))
     my_repo.index.commit('database update')
 print(my_repo.remotes.origin.push())
 
