8d0f7b51dd1355ea7fe6c22c0d1e5209ea84f785
isjustaname <50333654+isjustaname@users.noreply.github.com>
2024-06-07 10:54:03 +0700

    database update

--- MonteCarloRunner.py
+++ MonteCarloRunner.py
@@ -2,11 +2,11 @@ from MonteCarlo.MonteCarloSearcher import MonteCarloSearcher
 import git
 import os
 
-monte_carlo_searcher = MonteCarloSearcher()
-# monte_carlo_searcher.makeNewTree()
-for i in range(0, 10):
-    monte_carlo_searcher.loadTreeData()
-    monte_carlo_searcher.runAlgorihm(10)
+# monte_carlo_searcher = MonteCarloSearcher()
+# # monte_carlo_searcher.makeNewTree()
+# for i in range(0, 10):
+#     monte_carlo_searcher.loadTreeData()
+#     monte_carlo_searcher.runAlgorihm(10)
 
 my_repo = git.Repo(os.getcwd())
 if my_repo.is_dirty(untracked_files=True):
