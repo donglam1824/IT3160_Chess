b01428a01c3d1553ce4af7b17dfde1c12a7a12f6
donglam1824 <donglam0378378@gmail.com>
2024-06-08 15:38:39 +0700

    train

--- MonteCarloRunner.py
+++ MonteCarloRunner.py
@@ -6,7 +6,7 @@ monte_carlo_searcher = MonteCarloSearcher()
 #monte_carlo_searcher.makeNewTree()
 for i in range(0, 10):
     monte_carlo_searcher.loadTreeData()
-    monte_carlo_searcher.runAlgorihm(120)
+    monte_carlo_searcher.runAlgorihm(200)
 
 
 # my_repo = git.Repo(os.getcwd())
